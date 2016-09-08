aabe34a84f3cfd028eb486252f62b9e84cdab58e
danieldulaney <dulaney.daniel@gmail.com>
2016-09-08 01:40:23 -0400

    Finished first draft of observables.py
    * Added private utility functions
    * Documented private utility functions
    * Added function bodies for all class methods
    
    Code has not been run yet

--- observables/core/observable.py
+++ observables/core/observable.py
@@ -1,7 +1,7 @@
 class Observable(object):
 
     def __init__(self):
-        pass
+        self.__ensure_properties()
 
     def watch(self, *watchers):
         '''
@@ -11,6 +11,9 @@ class Observable(object):
             callback functions for update and complete
         '''
 
+        self.__ensure_properties()
+        self.__watchers |= self.__format_watchers(watchers)
+
     def unwatch(self, *watchers):
         '''
         Remove a watcher from an :py:class:Observable.
@@ -18,6 +21,9 @@ class Observable(object):
         :param *watchers: One or more 2-tuples, each containing
             callback functions for update and complete
         '''
+
+        self.__ensure_properties()
+        self.__watchers -= self.__format_watchers(watchers)
 
     def _update(self, payload):
         '''
@@ -31,6 +37,9 @@ class Observable(object):
         :type payload: any
         '''
 
+        for watcher in self.__watchers:
+            watcher[0](payload)
+
     def _finish(self):
         '''
         Alert all watchers that this observable will not produce more
@@ -39,3 +48,36 @@ class Observable(object):
         This private method is meant to be called by a subclass. It
         calls the finished function of each watcher.
         '''
+
+        for watcher in self.__watchers:
+            watcher[1]()
+
+    def __ensure_properties(self):
+        if not hasattr(self, "__watchers"):
+            self.__watchers = set()
+
+        if not hasattr(self, "__finished"):
+            self.__finished = False
+
+    def __format_watchers(self, raw_watchers):
+        '''
+        Turns the raw input from a *watchers parameter into a set of
+        homogenous (update, finish) tuples.
+
+        :param raw_watchers: The watcher list from a *watchers
+            parameter
+        :type raw_watchers: N-tuple
+        '''
+
+        nice_watchers = set()
+
+        for raw_watcher in raw_watchers:
+            update = raw_watcher[0]
+            try:
+                finish = raw_watcher[1]
+            except IndexError:
+                finish = None
+
+            nice_watchers.add((update, finish))
+
+        return nice_watchers
