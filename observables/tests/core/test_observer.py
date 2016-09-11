1374cb0a0bb74d7176bfccf8fa9ee78610001794
danieldulaney <dulaney.daniel@gmail.com>
2016-09-11 15:09:26 -0400

    Added tests for observer

---
+++ observables/tests/core/test_observer.py
@@ -0,0 +1,18 @@
+import unittest
+from core.observer import Observer
+
+
+class Test(unittest.TestCase):
+
+    def testAbstraction(self):
+        pass
+
+    def testSubclass(self):
+        pass
+
+    def testMixin(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
