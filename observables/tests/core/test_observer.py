430588ce374688e71aa54ddc82972b9b5f869e78
danieldulaney <dulaney.daniel@gmail.com>
2016-09-11 16:02:40 -0400

    Finished tests

--- observables/tests/core/test_observer.py
+++ observables/tests/core/test_observer.py
@@ -5,14 +5,36 @@ from core.observer import Observer
 class Test(unittest.TestCase):
 
     def testAbstraction(self):
-        pass
+        self.assertRaises(TypeError, Observer,
+                          "Observer should throw a TypeError on instantiation")
 
     def testSubclass(self):
-        pass
 
-    def testMixin(self):
-        pass
+        class ConcreteObserverNoUpdate(Observer):
 
+            def finish(self):
+                return True
+
+        class ConcreteObserverNoFinish(Observer):
+
+            def update(self, payload):
+                return payload
+
+        class ConcreteObserver(Observer):
+
+            def update(self, update):
+                return update
+
+            def finish(self):
+                return True
+
+        self.assertRaises(TypeError, ConcreteObserverNoUpdate,
+                          "ConcreteObserverNoUpdate should throw a TypeError on instantiation")
+
+        self.assertRaises(TypeError, ConcreteObserverNoFinish,
+                          "ConcreteObserverNoFinish should throw a TypeError on instantiation")
+
+        ConcreteObserver()
 
 if __name__ == "__main__":
     unittest.main()
